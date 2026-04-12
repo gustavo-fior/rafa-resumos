69cabab0b0baaa19fe998a22b00e9f96218f338d
Gustavo <gustavo_fior@outlook.com>
2026-04-12 14:45:05 -0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@rafa-resumos-2/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { env } from "@rafa-resumos-2/env/web";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_SERVER_URL,
+});
