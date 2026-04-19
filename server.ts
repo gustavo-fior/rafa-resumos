a0a83547f367565363b8541467af6132cc0c1996
Gustavo <gustavo_fior@outlook.com>
2026-04-19 20:50:25 -0300

    letsgo

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import "hono";
 
 // @ts-ignore Generated during the server build step before Vercel packages the function.
-import app from "./apps/server/dist/index.mjs";
+import app from "./apps/server/dist/main.mjs";
 
 export default app;
